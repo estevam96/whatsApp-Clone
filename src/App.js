334d9a7dde6442061cabdeb22ea08a4199dc7950
Marking96 <marcelo.estevam15@hotmail.com>
2018-02-16 02:06:46 -0300

    Iniciando Redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import Routes from './Routes';
+import reducers from './reducers';
 
 export default props => (
-    <Routes />
+    <Provider store={createStore(reducers)}>
+        <Routes />
+    </Provider>
 );
 
