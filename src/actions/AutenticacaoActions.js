55423ba56fce696f516e1a0c2d9ff9de8b7ade6e
Marking96 <marcelo.estevam15@hotmail.com>
2018-02-24 01:04:30 -0300

    preparando Login

--- src/actions/AutenticacaoActions.js
+++ src/actions/AutenticacaoActions.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
+import b64 from 'base-64';
 
 export const modificaEmail = texto => (
     {
@@ -25,7 +26,12 @@ export const modificarNome = texto => (
 export const cadastraUsuario = ({ nome, email, senha }) => (
      dispatch => {
         firebase.auth().createUserWithEmailAndPassword(email, senha)
-            .then(user => cadastroUsuarioSucesso(dispatch))
+            .then(user => {
+                let emailB64 = b64.encode(email);
+               firebase.database().ref(`/contato/${emailB64}`)
+                    .push({ nome })
+                    .then(value => cadastroUsuarioSucesso(dispatch))               
+            })
             .catch(erro => cadastroUsuarioErro(erro, dispatch));
     }
 
@@ -40,3 +46,13 @@ const cadastroUsuarioSucesso = (dispatch) => {
 const cadastroUsuarioErro = (erro, dispatch) => {
     dispatch({ type: 'cadastro_usuario_erro', payload: erro.message });
 };
+
+export const autenticarUsuario = ({ email, senha }) => {
+    firebase.auth().signInWithEmailAndPassword(email, senha)
+        .then()
+        .catch();
+
+    return {
+        type: 'teste'
+    }
+}
--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,51 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, TextInput, Button, TouchableHighlight, Image } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
-import { modificaEmail, modificaSenha } from '../actions/AutenticacaoActions';
+import { modificaEmail, modificaSenha, autenticarUsuario } from '../actions/AutenticacaoActions';
 
 const background = require('../img/bg.png');
 
-const formLogin = props => (
-    <Image style={{ flex: 1, width: null }} source={background} >
-        <View style={{ flex: 1, padding: 10 }}>
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ fontSize: 25, backgroundColor: 'transparent', color: '#fff' }} >
-                    WhatsApp Clone
-                </Text>
-            </View>
-            <View style={{ flex: 2, padding: 10 }}>
-                <TextInput
-                    value={props.email}
-                    style={{ fontSize: 20, height: 45 }}
-                    placeholder='E-mail'
-                    placeholderTextColor='#fff'
-                    onChangeText={texto => props.modificaEmail(texto)}
-                />
-                <TextInput
-                    secureTextEntry
-                    value={props.senha}
-                    style={{ fontSize: 20, height: 45 }}
-                    placeholder='Senha'
-                    placeholderTextColor='#fff'
-                    onChangeText={texto => props.modificaSenha(texto)}
-                />
-                <TouchableHighlight onPress={() => Actions.formCadastro()} >
-                    <Text style={{ fontSize: 20, color: '#fff' }} >
-                        Ainda não tem cadastro? cadastre-se
-                    </Text>
-                </TouchableHighlight>
-            </View>
-            <View style={{ flex: 2 }} >
-                <Button
-                    title='Acessar'
-                    onPress={() => false}
-                    color='#115E54'
-                />
-            </View>
-        </View>
-    </Image>
-);
+class formLogin extends Component {
+
+    _autenticarUsuario() {
+        const { email, senha } = this.props;
+        this.props.autenticarUsuario({ email, senha });
+    }
+
+    render() {
+        return (
+            <Image style={{ flex: 1, width: null }} source={background} >
+                <View style={{ flex: 1, padding: 10 }}>
+                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                        <Text 
+                            style={{ fontSize: 25, backgroundColor: 'transparent', color: '#fff' }} 
+                        >
+                            WhatsApp Clone
+                        </Text>
+                    </View>
+                    <View style={{ flex: 2, padding: 10 }}>
+                        <TextInput
+                            value={this.props.email}
+                            style={{ fontSize: 20, height: 45 }}
+                            placeholder='E-mail'
+                            placeholderTextColor='#fff'
+                            onChangeText={texto => this.props.modificaEmail(texto)}
+                        />
+                        <TextInput
+                            secureTextEntry
+                            value={this.props.senha}
+                            style={{ fontSize: 20, height: 45 }}
+                            placeholder='Senha'
+                            placeholderTextColor='#fff'
+                            onChangeText={texto => this.props.modificaSenha(texto)}
+                        />
+                        <TouchableHighlight onPress={() => Actions.formCadastro()} >
+                            <Text style={{ fontSize: 20, color: '#fff' }} >
+                                Ainda não tem cadastro? cadastre-se
+                            </Text>
+                        </TouchableHighlight>
+                    </View>
+                    <View style={{ flex: 2 }} >
+                        <Button
+                            title='Acessar'
+                            onPress={() => this._autenticarUsuario()}
+                            color='#115E54'
+                        />
+                    </View>
+                </View>
+            </Image>
+        );
+    }
+} 
 
 const mapStateToProps = state => (
     {
@@ -54,4 +66,8 @@ const mapStateToProps = state => (
     }
 );
 
-export default connect(mapStateToProps, { modificaEmail, modificaSenha })(formLogin);
+export default connect(mapStateToProps, { 
+                                    modificaEmail, 
+                                    modificaSenha, 
+                                    autenticarUsuario 
+                                })(formLogin);
