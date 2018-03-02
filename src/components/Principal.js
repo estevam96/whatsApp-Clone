8cc3c4e4565b02168c11f25f97cdb0551a453340
Marking96 <marcelo.estevam15@hotmail.com>
2018-03-02 00:13:35 -0300

    Tab-View

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import { View, Text, } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
+import TabBarMenu from './TabBarMenu';
 
-const Principal = props => (
-      <View style={{ marginTop: 100 }}>
-        <Text> Página Principal </Text>
-      </View>
+const initialLayout = {
+  height: 0,
+  width: Dimensions.get('window').width,
+};
+
+const Conversas = () => <View style={[ styles.container, { backgroundColor: '#ff4081' } ]} />;
+const Conatatos = () => <View style={[ styles.container, { backgroundColor: '#673ab7' } ]} />;
+
+export default class Principal extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'first', title: 'Conversas' },
+      { key: 'second', title: 'Conatatos' },
+    ],
+  };
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderHeader = props => <TabBarMenu {...props} />;
+
+  _renderScene = SceneMap({
+    first: Conversas,
+    second: Conatatos,
+  });
+
+  render() {
+    return (
+      <TabViewAnimated
+        style={styles.container}
+        navigationState={this.state}
+        renderScene={this._renderScene}
+        renderHeader={this._renderHeader}
+        onIndexChange={this._handleIndexChange}
+        initialLayout={initialLayout}
+      />
     );
+  }
+}
 
-export default Principal;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
